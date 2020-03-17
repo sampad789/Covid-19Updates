81947ed63e8b917a4a772a90a1f8b87afc1d9784
sampad789 <sampad789@gmail.com>
2020-03-17 22:09:36 +0200

    finished with changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
 
-function App() {
+import React from "react";
+import Stats from "./components/Stats";
+import CountrySelector from "./components/CountrySelector";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      <h2>Worldwide Stats of COVID-19 Virus: </h2>
+      <Stats url="https://covid19.mathdro.id/api" />
+      <CountrySelector />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Stats.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import useStats from "../utils/useStats";
+
+const StatGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+`;
+const StatBlock = styled.div`
+  background: black;
+  font-size: 2rem;
+  color: #ff6ec7;
+  padding: 2rem;
+  border-radius: 2rem;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  text-align: center;
+  margin: 2rem;
+`;
+
+export default function Stats({ url }) {
+  const { stats, loading, error } = useStats(url);
+  // console.log(stats, loading, error);
+  if (loading) return <h1>Loading ...</h1>;
+  return (
+    <>
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <StatGrid>
+          <StatBlock>
+            <h3>Confirmed:</h3>
+            <span>{stats.confirmed ? stats.confirmed.value : "NA"}</span>
+          </StatBlock>
+          <StatBlock>
+            <h3>Deaths:</h3>
+            <span>{stats.deaths ? stats.deaths.value : "NA"}</span>
+          </StatBlock>
+          <StatBlock>
+            <h3>Recovered:</h3>
+            <span>{stats.recovered ? stats.recovered.value : "NA"}</span>
+          </StatBlock>
+        </StatGrid>
+      )}
+    </>
+  );
+}
